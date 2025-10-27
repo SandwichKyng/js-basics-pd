18301036e624c1dbf49ff25c816c302e18541b0c
unknown <you@example.com>
2025-10-27 11:26:31 +0200

    third commit

---
+++ people-array.js
@@ -0,0 +1,8 @@
+
+
+let people = [
+  { name: "Jānis", age: 12, isStudent: true},
+  { name: "Oskars", age: 21, isStudent: false}
+];
+
+for (let people)
---
+++ ticket-price-function.js
@@ -0,0 +1,12 @@
+
+
+let visitorAge = 12;
+let isStudent = true;
+let cena = 10;
+
+
+function printTicketPrice (visitorAge, isStudent) {
+    return visitorAge;
+}
+
+console.log(printTicketPrice(visitorAge, isStudent))
