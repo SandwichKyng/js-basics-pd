d8f9245405adb6ba293bb21ca03ec1d6b1e38f1b
unknown <you@example.com>
2025-10-27 11:36:05 +0200

    ceturtais komits

---
+++ internet-store.js
@@ -0,0 +1,8 @@
+
+let preces = [
+  { prece: "dators", kategorija: "elektronika", cena: 700, daudzums: 200},
+  { prece: "telefons", kategorija: "elektronika", cena: 150, daudzums: 50},
+  { prece: "cepure", kategorija: "drēbes", cena: 5, daudzums: 150}
+];
+
+console.log(kategorija)
--- ticket-price-function.js
+++ ticket-price-function.js
@@ -6,7 +6,13 @@ let cena = 10;
 
 
 function printTicketPrice (visitorAge, isStudent) {
-    return visitorAge;
+    return visitorAge <= 12;
+}
+
+console.log(printTicketPrice(visitorAge, isStudent))
+
+function printTicketPrice (visitorAge, isStudent) {
+    return visitorAge >= 35;
 }
 
 console.log(printTicketPrice(visitorAge, isStudent))
